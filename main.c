e3a93ab306ddf958fff68dd241bdaaf2250a6325
Olivier-flx <olivier.filloux@live.fr>
2025-01-20 22:23:45 +0100

    supp file for valgrind

--- main.c
+++ main.c
@@ -6,16 +6,19 @@
 /*   By: ofilloux <ofilloux@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 16:23:22 by ofilloux          #+#    #+#             */
-/*   Updated: 2025/01/20 18:09:59 by ofilloux         ###   ########.fr       */
+/*   Updated: 2025/01/20 22:18:42 by ofilloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./header/minishell.h"
 
+
+// HOW TO RUN //
+// valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --suppressions=readline.supp -s ./mini
+
 int	process_line(char **line)
 {
 	printf("%s\n", *line);
-	free (*line);
 	return (0);
 }
 
@@ -24,6 +27,9 @@ int	run_minishell(t_data	*data)
 	char		*line;
 	t_tok_line	*token_list;
 
+	token_list = malloc(sizeof(t_tok_line));
+	if (!token_list)
+		return (1);
 	while (true && data->env)
 	{
 		line = readline("minishell> ");
@@ -31,6 +37,7 @@ int	run_minishell(t_data	*data)
 		{
 			create_input_token(line, &token_list);
 		//	add_history(line);
+		//	clear_history();
 			process_line(&line);
 			free(line);
 		}
@@ -38,6 +45,7 @@ int	run_minishell(t_data	*data)
 			continue ;
 		break ; // to remove when more advanced
 	}
+	free(token_list);
 	return (0);
 }
 
--- utils_string/create_input_token.c
+++ utils_string/create_input_token.c
@@ -6,15 +6,49 @@
 /*   By: ofilloux <ofilloux@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 17:57:45 by ofilloux          #+#    #+#             */
-/*   Updated: 2025/01/20 17:59:28 by ofilloux         ###   ########.fr       */
+/*   Updated: 2025/01/20 18:44:02 by ofilloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
+int	s_len(const char *s)
+{
+	int	i;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*s_dup(char *s)
+{
+		char	*cpy;
+		int	 i = 0;
+
+		if(!s)
+				return (NULL);
+		cpy = malloc((s_len(s) + 1) * sizeof(char));
+		if (!cpy)
+				return (NULL);
+		while(s[i])
+		{
+				cpy[i] = s[i];
+				i++;
+		}
+		cpy[i] = '\0';
+		return (cpy);
+}
+
 int	create_input_token (char *src, t_tok_line **line)
 {
+	//(*line)->word = s_dup(src);
 	(*line)->word = src;
 	printf("token test = %s\n", (*line)->word);
+
+	//free ((*line)->word);
 	return (0);
 }
