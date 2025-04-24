36dfbe741320cacf3a0a3410a49f574e1c44065f
Olivier-flx <olivier.filloux@live.fr>
2025-04-24 13:24:10 +0200

    starting exec

--- src/exec/create_files.c
+++ src/exec/create_files.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec.c                                             :+:      :+:    :+:   */
+/*   create_files.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ofilloux <ofilloux@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/24 10:30:25 by ofilloux          #+#    #+#             */
-/*   Updated: 2025/04/24 11:31:56 by ofilloux         ###   ########.fr       */
+/*   Created: 2025/04/24 11:26:59 by ofilloux          #+#    #+#             */
+/*   Updated: 2025/04/24 11:31:58 by ofilloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/minishell.h"
 
-int main_exec(t_data *data)
+int create_files(t_data *data)
 {
-	create_files(data);
+	//get pwd
+	// loop over i_node to get the files
 }
