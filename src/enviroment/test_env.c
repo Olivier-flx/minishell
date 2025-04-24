9cd07b6c081688f7096a0a0b0535039836abea22
Olivier-flx <olivier.filloux@live.fr>
2025-04-24 13:49:21 +0200

    delete useless file

--- src/enviroment/test_env.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    char *path = getenv("PATH");
-    if (path) printf("PATH EN C: %s\n", path);
-    else printf("PATH no encontrado\n");
-    return 0;
-}
