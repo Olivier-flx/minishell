95e7502063598af7044712304f1d4f0796e8d245
Olivier Filloux <ofilloux@student.42barcelona.com>
2025-06-18 19:17:02 +0200

    :memo: [DOC] Adding documentations for signals

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ofilloux <ofilloux@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: ofilloux <ofilloux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:28:21 by marvin            #+#    #+#             */
-/*   Updated: 2025/06/09 11:02:52 by ofilloux         ###   ########.fr       */
+/*   Updated: 2025/06/18 18:53:17 by ofilloux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,29 @@ void	signal_handlers_for_readline(t_data *data)
 	}
 }
 
-// cat /usr/include/readline/readline.h for RL_STATE_READCMD
+/**
+ * @brief Signal handler for SIGINT and SIGQUIT signals.
+ *
+ * - On SIGINT (e.g., Ctrl+C):
+ *   - Sets the global variable `g_signal_received` to 130 (custom exit code).
+ *   - If Readline is currently reading a command (checked using
+ *     `rl_readline_state & RL_STATE_READCMD`), it refreshes the prompt
+ *     using `rl_redisplay()` to show the clean line.
+ *
+ * - On SIGQUIT (e.g., Ctrl+\):
+ *   - Only handled if Readline is NOT actively reading a command, i.e.
+ *     `!(rl_readline_state & RL_STATE_READCMD)` is true.
+ *     Happens when a child process is running, not during prompt input.
+ *   - In this case, sets `g_signal_received` to 131 and prints
+ *     "Quit (core dumped)" to stdout.
+ *
+ * @param sig The signal number received (SIGINT or SIGQUIT).
+ * @note cat /usr/include/readline/readline.h for RL_STATE_READCMD
+ *           and rl_readline_state
+ *           rl_readline_state is a global variable set by readline library,
+ *           RL_STATE_READCMD is a binary defined in readline library.
+ *
+ */
 void	handle_signal(int sig)
 {
 	if (sig == SIGINT)
@@ -45,26 +67,45 @@ void	handle_signal(int sig)
 	}
 }
 
+/**
+ * @brief Configure custom signal handlers for the shell session.
+ *
+ * Sets up signal handling for an interactive shell:
+ * - SIGINT (Ctrl+c)
+ * - SIGQUIT (Ctrl+\)
+ * - SIGTSTP (Ctrl+Z)
+ *
+ * Set sa.sa_mask to emptyset so no signal are bloqued during handler exec
+ *
+ * @see handle_signal
+ * @see /usr/include/asm-generic/signal-defs.h for SIG_IGN
+ */
 void	setup_signals(void)
 {
 	struct sigaction	sa;
 
 	sa.sa_handler = handle_signal;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
+	sa.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa, NULL);
 	sa.sa_handler = SIG_IGN;
 	sigaction(SIGQUIT, &sa, NULL);
 	sigaction(SIGTSTP, &sa, NULL);
 }
 
+/**
+ * @brief Restore signals to defauld in case of being in a child process
+ *        and not in a readline anymore.
+ *
+ *       - sa.sa_handler = SIG_DFL; restore handler to default
+ */
 void	reset_signals_to_default(void)
 {
 	struct sigaction	sa;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_handler = SIG_DFL;
-	sa.sa_flags = 0;
+	sa.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGQUIT, &sa, NULL);
 }
